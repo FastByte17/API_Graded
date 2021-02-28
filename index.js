16939d21d082110d480dd377849b32be73e5a7e2
Nabeel <nabeelbaig962@gmail.com>
2021-03-01 04:50:01 +0500

    Installed multer and added the route to index.js.

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const imageUpload = require('./routes/images');
+
+app.use('/uploadImage', imageUpload);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
