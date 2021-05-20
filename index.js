f40cc07205af0952d1fd5100eff16a5dc44d5f95
Nabeel <nabeelbaig962@gmail.com>
2021-05-20 04:10:34 +0300

    Added register

--- index.js
+++ index.js
@@ -4,10 +4,27 @@ const port = 3000
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
-const imageUpload = require('./routes/images');
 
-app.use('/uploadImage', imageUpload);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
+})
+
+
+app.post('/register', (req, res) => {
+  if ('username' in req.body == false ) {
+    res.status(400);
+    res.json({status: "username missing"})
+    return;
+  }
+  if('password' in req.body == false) {
+    res.status(400);
+    res.json({status: "password missing"})
+    return;
+  }
+  if ('email' in req.body == false) {
+    res.status(400);
+    res.json({status: "email missing"})
+    return;
+  }
 })
